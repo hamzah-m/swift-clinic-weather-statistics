3fc3bc5e83d3fe930872e3365b5576c3792e435c
Hamzah Mugharbil <h.mugharbil@gmail.com>
2020-07-21 02:56:58 -0400

    Initial commit at project creation.

---
+++ WeatherStatistics/WeatherStatistics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherStatistics
+//
+//  Created by Hamzah Mugharbil on 7/21/20.
+//  Copyright © 2020 Hamzah Mugharbil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
